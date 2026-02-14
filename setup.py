e0dcc964cc402a3bf08c68f350ff42cd920ae000
Aarushi Dua <aarudua@iu.edu>
2026-02-14 11:23:27 -0500

    add rag content

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Aarushi Dua",
+    author_email="aarushidua4th@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
